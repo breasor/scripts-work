0f7b799577ce80d4e1c6fab414d67375c05db8b7
Brett Reasor <brett.reasor@gmail.com>
2020-03-13 16:51:20 -0400

    initial

--- agent stations/remote-bginfo.ps1
+++ agent stations/remote-bginfo.ps1
@@ -1,21 +1,20 @@
-
+#!ps1
+#timeout=90000
+$VerbosePreference = "continue"
+Import-Module BitsTransfer
 
 $bgInfoFolder = "C:\BgInfo"
-$bgInfoFolderContent = $bgInfoFolder + "\*"
-$itemType = "Directory"
 $bgInfoUrl = "https://download.sysinternals.com/files/BGInfo.zip"
-$bgInfoZip = "C:\BgInfo\BGInfo.zip"
-$bgInfoEula = "C:\BgInfo\Eula.txt"
-$logonBgiUrl = "https://tinyurl.com/yxlxbgun"
-$logonBgiZip = "C:\BgInfo\LogonBgi.zip"
-$bgInfoRegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
-$bgInfoRegKey = "BgInfo"
-$bgInfoRegType = "String"
-$bgInfoRegKeyValue = "C:\BgInfo\Bginfo64.exe C:\BgInfo\logon.bgi /timer:0 /nolicprompt"
-$regKeyExists = (Get-Item $bgInfoRegPath -EA Ignore).Property -contains $bgInfoRegkey
-$writeEmptyLine = "`n"
-$writeSeperator = " - "
-$time = Get-Date -UFormat "%A %m/%d/%Y %R"
-$foregroundColor1 = "Yellow"
-$foregroundColor2 = "Red"
- 
+$logonBgiUrl = "https://ansastatic.blob.core.windows.net/remote-setup/logon.bgi"
+
+Write-Verbose "Download Started"
+New-Item -ItemType Directory -Force -Path $bgInfoFolder
+Start-BitsTransfer -Source $bgInfoUrl -Destination $env:TEMP\bginfo.zip
+Write-Verbose "Extracting Archive"
+Expand-Archive -LiteralPath $env:TEMP\bginfo.zip -DestinationPath $bgInfoFolder -Force
+Remove-Item $env:TEMP\bginfo.zip
+Remove-Item $bgInfoFolder\eula.txt
+Write-Verbose "Downloading bgi script"
+Invoke-WebRequest -Uri $logonBgiUrl -OutFile $bgInfoFolder\logon.bgi
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name BgInfo -PropertyType String -Value "C:\BgInfo\Bginfo64.exe C:\BgInfo\logon.bgi /timer:0 /nolicprompt"
+Start-Process "$bgInfoFolder\bginfo64.exe" -ArgumentList "$bgInfoFolder\logon.bgi /timer:0 /nolicprompt" -Wait -NoNewWindow
