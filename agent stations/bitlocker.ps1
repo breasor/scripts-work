15cea4b2b08a8ac3852e2ecb26f08a43e5f04bd2
Brett Reasor <breasor@users.noreply.github.com>
2020-03-15 16:18:02 -0400

    Update bitlocker.ps1

--- agent stations/bitlocker.ps1
+++ agent stations/bitlocker.ps1
@@ -4,3 +4,14 @@ $RecoveryDisk = (Get-WmiObject -Query "Select * From Win32_LogicalDisk" | ? { $_
 New-Item -ItemType Directory -Path $recoverydisk\Recovery -Force
 Get-BitLockerVolume C: | Enable-BitLocker -SkipHardwareTest -EncryptionMethod Aes128 -RecoveryKeyPath "$RecoveryDisk\Recovery" -RecoveryKeyProtector
 Restart-Computer -Delay 15
+
+# Export the BitLocker recovery keys for all drives and display them at the Command Prompt.
+$BitlockerVolumers = Get-BitLockerVolume
+$BitlockerVolumers |
+ForEach-Object {
+$MountPoint = $_.MountPoint
+$RecoveryKey = [string]($_.KeyProtector).RecoveryPassword
+if ($RecoveryKey.Length -gt 5) {
+Write-Output ("The drive $MountPoint has a BitLocker recovery key $RecoveryKey.")
+}
+}
