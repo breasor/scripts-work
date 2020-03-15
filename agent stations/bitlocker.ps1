eac6aab16b78835624d047218507ffc1ae2693a9
Brett Reasor <breasor@users.noreply.github.com>
2020-03-15 18:06:27 -0400

    Update bitlocker.ps1

--- agent stations/bitlocker.ps1
+++ agent stations/bitlocker.ps1
@@ -1,17 +1,10 @@
-# Format USB key and label it RECOVERYKEY
-
-$RecoveryDisk = (Get-WmiObject -Query "Select * From Win32_LogicalDisk" | ? { $_.driveType -eq 2 -and $_.volumename -eq "RECOVERYKEY"}).DeviceID
-New-Item -ItemType Directory -Path $recoverydisk\Recovery -Force
-Get-BitLockerVolume C: | Enable-BitLocker -SkipHardwareTest -EncryptionMethod XtsAes128 -RecoveryKeyPath "$RecoveryDisk\Recovery" -RecoveryKeyProtector
-Restart-Computer -Delay 15
-
 # Export the BitLocker recovery keys for all drives and display them at the Command Prompt.
-$BitlockerVolumers = Get-BitLockerVolume
-$BitlockerVolumers |
-ForEach-Object {
-$MountPoint = $_.MountPoint
-$RecoveryKey = [string]($_.KeyProtector).RecoveryPassword
-if ($RecoveryKey.Length -gt 5) {
-Write-Output ("The drive $MountPoint has a BitLocker recovery key $RecoveryKey.")
-}
-}
+$RecoveryDisk = (Get-WmiObject -Query "Select * From Win32_LogicalDisk" | ? { $_.driveType -eq 2 }).DeviceID
+New-Item -ItemType Directory -Path $recoverydisk\Recovery -Force
+$SerialNo = (Get-CimInstance -ClassName Win32_Bios).serialnumber
+$BitlockerVolume = Get-BitLockerVolume C:
+$KeyProtectors = @($BitlockerVolume.KeyProtector)
+
+$KeyProtectors | where {$_.KeyProtectorType -eq "RecoveryPassword"} | Out-File "$RecoveryDisk\recovery\$serialNo recovery keys.txt"
+
+
