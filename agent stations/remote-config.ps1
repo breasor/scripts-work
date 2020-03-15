d7a5661db2f3f28990257efda823de8f5d44c873
Brett Reasor <breasor@users.noreply.github.com>
2020-03-15 16:01:34 -0400

    Update remote-config.ps1

--- agent stations/remote-config.ps1
+++ agent stations/remote-config.ps1
@@ -215,37 +215,6 @@ $Shortcut.Arguments = "/v:oc-endirds1.endicottcomm.internal"
 $Shortcut.Save()
 
 
-# Software Installs
-$VerbosePreference = "continue"
-Import-Module BitsTransfer
-
-# Download & install startel softphone
-$softwareUrl = "https://github.com/breasor/scripts-work/raw/master/agent%20stations/Endicott/SoftPhoneSetup.msi"
-Start-BitsTransfer -Source $softwareUrl -Destination $env:TEMP\SoftPhoneSetup.msi
-Start-Process "msiexec.exe" -ArgumentList "/i $env:TEMP\SoftPhoneSetup.msi /qn"
-
-# Download & install ResponderSetup.msi
-$softwareUrl = "https://github.com/breasor/scripts-work/raw/master/agent%20stations/Endicott/ResponderSetup.msi"
-Start-BitsTransfer -Source $softwareUrl -Destination $env:TEMP\ResponderSetup.msi
-Start-Process "msiexec.exe" -ArgumentList "/i $env:TEMP\ResponderSetup.msi /qn"
-
-
-
-# Download & install Silverlight
-$VerbosePreference = "continue"
-$softwareUrl = "https://github.com/breasor/scripts-work/raw/master/agent%20stations/Endicott/Silverlight_x64.exe"
-Start-BitsTransfer -Source $softwareUrl -Destination $env:TEMP\Silverlight_x64.exe
-Start-Process "$env:temp\Silverlight_x64.exe" -ArgumentList "/q"
-
-
-# Download & install SophosSetup.exe
-$VerbosePreference = "continue"
-$softwareUrl = "https://github.com/breasor/scripts-work/raw/master/agent%20stations/Endicott/SophosSetup.exe"
-Start-BitsTransfer -Source $softwareUrl -Destination $env:TEMP\SophosSetup.exe
-Start-Process "$env:temp\SophosSetup.exe" -ArgumentList '--quiet --devicegroup="CSR Computers"'
-
-New-Item -ItemType Directory -Path E:\Recovery -Force
-Get-BitLockerVolume | Enable-BitLocker -SkipHardwareTest -EncryptionMethod Aes128 -RecoveryKeyPath "E:\Recovery" -RecoveryKeyProtector
 
 # add remote user
 net user Remote /add
