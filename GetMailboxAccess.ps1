4a7be242fa7ecccbb190ecb765f0b045ee5fc9f5
Brett Reasor <brett.reasor@gmail.com>
2020-03-23 17:30:45 -0400

    cleanup

---
+++ GetMailboxAccess.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.SYNOPSIS
+Returns a list of users with full access permissions to shared mailboxes.
+
+.PARAMETER NoAccess
+Using this switch will only return mailboxes that do not have any user assigned for full access.
+
+.EXAMPLE
+.\GetMailboxAccess.ps1 -NoAccess
+#>
+
+param(
+
+[switch]$NoAccess
+)
+#requires -version 2
+
+$Result = @(); 
+$sharedMailboxes = get-mailbox -filter { IsShared -eq $true }
+foreach($mailbox in $sharedMailboxes)
+{
+    $permissionsResult = New-Object System.Collections.ArrayList
+    $permissions =  $mailbox | Get-MailboxPermission | ? { $_.AccessRights -match "FullAccess" } 
+    $permissions | % { if ($_.Deny -eq $false -and $permissionsResult -notcontains $_.User) { $permissionsResult.Add($_.User.ToString()) | Out-Null } }
+    $permissions | % { if ($_.Deny -eq $true -and $permissionsResult -contains $_.User) { $permissionsResult.Remove($_.User.ToString()) | Out-Null } }
+    if ($NoAccess)
+    {
+        if ($permissionsResult.Count -eq 0)
+        {
+            New-Object PSObject -Property @{"Alias" = $mailbox.Alias; "EmailAddress" = $mailbox.PrimarySmtpAddress.ToString(); "FullAccess" = $permissionsResult}
+        }
+    } else {
+        New-Object PSObject -Property @{"Alias" = $mailbox.Alias; "EmailAddress" = $mailbox.PrimarySmtpAddress.ToString(); "FullAccess" = $permissionsResult}
+    }
+}
--- snippits.ps1
+++ snippits.ps1
@@ -61,5 +61,11 @@ Get-Process -Id (Get-NetTCPConnection -LocalPort $port).OwningProcess
 #computer uptime
 Get-CimInstance Win32_OperatingSystem | select-object csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
 
+# Enable Remote Desktop
+New-PSSession -ComputerName "Computer"
+(Get-WmiObject Win32_TerminalServiceSetting -Namespace root\cimv2\TerminalServices).SetAllowTsConnections(1,1) | Out-Null
+(Get-WmiObject -Class "Win32_TSGeneralSetting" -Namespace root\cimv2\TerminalServices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0) | Out-Null
+Get-NetFirewallRule -DisplayName "Remote Desktop*" | Set-NetFirewallRule -enabled true
+
 
 #endregion
